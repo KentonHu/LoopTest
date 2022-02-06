f7de128d0a840c319fba75c2c98a4215f246fff8
Kenton <hutianyu0716@gmail.com>
2022-02-06 18:34:10 +0800

    LoopTest

---
+++ LoopTest.java
@@ -0,0 +1,71 @@
+/*
+	三种循环的区别
+ */
+
+public class LoopTest {
+	public static void main(String[] args) {
+
+		/*
+		//for循环
+		for(int i = 3;i<3;i++){
+			System.out.println("HelloWorld");
+		}
+		System.out.println("-----------------");
+
+		//while循环
+		int j=3;
+		while(j<3){
+			System.out.println("HelloWorld");
+			j++;
+		}
+		System.out.println("-----------------");
+
+		//do...while循环
+		int k=3;
+		do{
+			System.out.println("HelloWorld");
+			k++;
+		}while(k<3);
+		*/
+	
+
+
+		/*
+		//for循环
+		for(int i = 1;i<3;i++){
+
+			System.out.println("HelloWorld");
+		}
+		//System.out.println(i);
+		//for里定义的i不能在外面使用
+		System.out.println("-----------------");
+
+		//while循环
+		int j=1;
+		while(j<3){
+			System.out.println("HelloWorld");
+			j++;
+		}
+		System.out.println(j);
+		System.out.println("-----------------");
+		*/
+		
+
+
+		//死循环
+		
+		/*
+		for(;;){
+			System.out.println("for");
+		}
+
+		while(true){
+			System.out.println("while")
+		}
+
+		do{
+			System.out.println("do...while")
+		}while(true);
+		*/
+	}
+}
